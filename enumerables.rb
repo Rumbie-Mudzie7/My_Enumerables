e209cc8e9fce7f396d73a26af86a860464384bb4
vmwhoami <vmwhoami@gmail.com>
2020-06-30 12:23:17 +0300

    Solve the my select and my_each with index

--- enumerables.rb
+++ enumerables.rb
@@ -2,10 +2,41 @@ module Enumerable
   def my_each
     return enum_for unless block_given?
 
-    for item in self 
-      yield(item)
+    i = 0
+    while i < size
+      yield(to_a[i])
+      i += 1
     end
+  end
+
+  # My_each with index
+  def my_each_with_index
+    return enum_for unless block_given?
+
+    i = 0
+    while i < size
+      yield(to_a[i], i)
+      i += 1
+    end
+  end
+  # My_SELECT
+
+  def my_select
+    return enum_for unless block_given?
+
+    if is_a? Hash
+      selected = []
+      arr = to_a
+      arr.my_each { |el| selected << el if yield(el) }
+      return selected.to_h
+    end
+    selected = []
+    arr = to_a
+    arr.my_each { |el| selected << el if yield(el) }
+    selected
   end
 end
 
-
+# ha = { 'help' => 'me', 'learn' => 'smth' }
+# arr = [2, 4, 6, 9, 7, 10, 4]
+# p arr.my_select { |el| el == 4 }
--- tests.rb
+++ tests.rb
@@ -1,10 +1,10 @@
-require_relative "enumerables.rb"
+require_relative 'enumerables.rb'
 # (1..5).my_each do |item|
 #     puts item
 #  end
 ar = []
 has = { 'hello' => 'buy', 'true' => 'false' }
-has.my_each {|k,v| ar<< v}
+has.my_each { |_k, v| ar << v }
 
 # has.each  {|k,v| ar<< v}
 p ar
