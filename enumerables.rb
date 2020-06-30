1b8d1934b182c2da16ecab2bd383c97f287989c2
vmwhoami <vmwhoami@gmail.com>
2020-06-30 18:56:03 +0300

    Solve the no test pass for my_any method

--- enumerables.rb
+++ enumerables.rb
@@ -26,6 +26,7 @@ module Enumerable
 
   def my_select
     return enum_for unless block_given?
+
     if is_a? Hash
       selected = []
       arr = to_a
@@ -39,22 +40,26 @@ module Enumerable
   end
 
   # My_all?
-  def my_all?(_arg = nil)
-    if !block_given?
-      my_select { |item| return false unless item }
-    else
-      my_select { |item| return false unless yield(item) }
-    end
+  def my_all?(args = nil)
+    my_select { |el| return false unless el.match?(args) } if args.class == Regexp
+    my_select { |el| return false unless el.is_a?(args) } if args.class == Class
+    !block_given? ? my_select { |item| return false unless item } : my_select { |item| return false unless yield(item) }
     true
   end
 
   # My_any
-  def my_any?
-    if !block_given?
-      my_select { |item| return true if item }
-    else
-      my_select { |item| return true if yield(item) }
+  def my_any?(args = nil)
+    if args.class == Regexp
+      ar = []
+      my_each { |el| ar << el if el.match?(args) }
+      return  ar.empty? ? false : true
     end
+
+    my_select { |el| return true if el.is_a?(args) } if args.class == Class
+    unless block_given?
+      my_select { |item| return true if item }
+    end
+    my_select { |item| return true if yield(item) }
     false
   end
 
@@ -70,9 +75,9 @@ module Enumerable
   end
 
   # MY_COUNT
-  def my_count(_arg = nil)
-    count
-  end
+  # def my_count(_args = nil)
+  #   count
+  # end
 end
 
 # ha = { 'help' => 'smth', 'learn' => 'wede' }
--- tests.rb
+++ tests.rb
@@ -1,26 +1,15 @@
 require_relative 'enumerables.rb'
 
-module Enumerable
-    def ding?(arg = nil)
-      if arg.class == Regexp
-        my_select{|el| return false if !arg.match(el)}
-      else
-    
-      end
 
-        if !block_given?
-          my_select { |item| return false unless item }
-        else
-          my_select { |item| return false unless yield(item) }
-        end
-        true
-    end
-end
 
-# p %w[ant bear cat].ding? { |word| word.length >= 3 } #=> true
-# p %w[ant bear cat].ding? { |word| word.length >= 4 } #=> false
-p [[121],[3],4].ding?(/d/)                 #=> true
-# p [nil, true, 99].ding?                              #=> false
-# p [].ding?                                           #=> true
+# p "bear".match?(/bear/)
 
-# p /ack/.match('haystack') #=> #<MatchData "y">
+# None tests
+# %w{ant bear cat}.none? { |word| word.length == 5 } #=> true
+# %w{ant bear cat}.none? { |word| word.length >= 4 } #=> false
+# %w{ant bear cat}.none?(/plant/)                        #=> true
+# [1, 3.14, 42].none?(Float)                         #=> false
+# [].none?                                           #=> true
+# [nil].none?                                        #=> true
+# [nil, false].none?                                 #=> true
+# [nil, false, true].none?                           #=> false
