d64fe0d1d32c3d3a61401281f7ac261ba5ab18fd
rumbie <rumbiemudzie@gmail.com>
2020-06-30 10:36:35 +0200

    Create the first method

--- enumerables.rb
+++ enumerables.rb
@@ -1,1 +1,11 @@
-# Start on enumerables
+module Enumerable
+  def my_each
+    return enum_for unless block_given?
+
+    for item in self 
+      yield(item)
+    end
+  end
+end
+
+
---
+++ tests.rb
@@ -0,0 +1,10 @@
+require_relative "enumerables.rb"
+# (1..5).my_each do |item|
+#     puts item
+#  end
+ar = []
+has = { 'hello' => 'buy', 'true' => 'false' }
+has.my_each {|k,v| ar<< v}
+
+# has.each  {|k,v| ar<< v}
+p ar
